d0d0cc66a22a9295577663563160f9b2de388459
lucas.sousa <lucas.sousa@codificar.com.br>
2023-08-03 11:27:57 -0300

    first commit

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return [
+            [
+                'name' => 'lucas',
+                'age' => 30
+            ],
+            [
+                'name' => 'marcos',
+                'age' => 28
+            ]
+        ];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request;
+
+        return $data;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
